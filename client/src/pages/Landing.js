5d07c1fcd0d718d91e2df944d6d6b862a6d62dfc
Arezki <mmezrag@techitaway.co>
2023-03-04 15:39:16 +0100

    landing page structure

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,6 +2,27 @@ import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <h4>Landing Page</h4>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='jobdz' className='logo' />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
+  );
 };
 export default Landing;
