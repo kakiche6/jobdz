cc140e70f3e1984916beef49f3a8ff32d5acb825
Arezki <mmezrag@techitaway.co>
2023-03-07 20:52:39 +0100

    connect to mongodb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 const app = express();
 import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
 
 dotenv.config();
 
@@ -15,4 +16,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
