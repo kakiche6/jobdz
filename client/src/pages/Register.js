0aabb4e225013101754b140cb6cafa4825f3059d
Arezki <mmezrag@techitaway.co>
2023-03-07 20:09:58 +0100

    display alert complete

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -14,7 +14,7 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
-  const { isLoading, showAlert } = useAppContext();
+  const { isLoading, showAlert, displayAlert } = useAppContext();
 
   // global context and useNavigate later
 
@@ -23,12 +23,16 @@ const Register = () => {
   };
 
   const handleChange = e => {
-    console.log(e.target);
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
   };
 
   return (
