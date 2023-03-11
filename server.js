1da97be2a4ec60521a206c015c6cbcddadb4ebdf
Arezki <mmezrag@techitaway.co>
2023-03-11 13:48:22 +0100

    auth structure

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ import notFoundMiddleware from "./middleware/not-found.js";
 const app = express();
 import dotenv from "dotenv";
 import connectDB from "./db/connect.js";
+import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
