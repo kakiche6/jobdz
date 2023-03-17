43af923d6292a8adcd0d1640d2e1a9d94742e34e
Arezki <mmezrag@techitaway.co>
2023-03-17 22:10:31 +0100

    login user complete

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -10,6 +10,34 @@ import {
 } from "./actions";
 
 const reducer = (state, action) => {
+  if (action.type === LOGIN_USER_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      user: action.payload.user,
+      token: action.payload.token,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: "success",
+      alertText: "Login Successful! Redirecting...",
+    };
+  }
+  if (action.type === LOGIN_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
+    };
+  }
   if (action.type === REGISTER_USER_BEGIN) {
     return { ...state, isLoading: true };
   }
