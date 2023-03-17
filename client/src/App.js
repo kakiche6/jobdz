46ca051a23a8cdb43f66446e2235e259d1c6d041
Arezki <mmezrag@techitaway.co>
2023-03-17 22:33:00 +0100

    dashboard routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dashboard, Landing, Error, Register } from "./pages";
+import { Landing, Error, Register } from "./pages";
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />}></Route>
+          <Route path='add-job' element={<AddJob />}></Route>
+          <Route path='profile' element={<Profile />}></Route>
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
