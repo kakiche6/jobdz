db4ba37c971a0deaa3ee89b6104656d524527296
Arezki <mmezrag@techitaway.co>
2023-03-17 20:27:47 +0100

    persist user in local storage

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -11,14 +11,19 @@ import {
   REGISTER_USER_SUCCESS,
 } from "./actions";
 
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
+const userLocation = localStorage.getItem("location");
+
 export const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
-  user: null,
-  token: null,
-  userLocation: "",
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || "",
+  jobLocation: userLocation || "",
 };
 const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
@@ -39,6 +44,18 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  const addUserToLocalStorage = ({ user, token, location }) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", location);
+  };
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    localStorage.removeItem("location");
+  };
+
   const registerUser = async currentUser => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
@@ -55,11 +72,11 @@ const AppProvider = ({ children }) => {
       });
 
       // will add later
-      // addUserToLocalStorage({
-      //   user,
-      //   token,
-      //   location,
-      // })
+      addUserToLocalStorage({
+        user,
+        token,
+        location,
+      });
     } catch (error) {
       console.log(error.response);
       dispatch({
